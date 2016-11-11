9504be14994e6b6440c61ca855c29b0513e15dba
nwinden <noah.winden@gmail.com>
2016-11-10 18:38:17 -0600

    break it sean

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Card} from './Card'
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  GameCard : Card = {Name: "Sean"};
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/card/card.ts
@@ -0,0 +1,27 @@
+export class Card {
+
+     Name : String;
+
+    //  Effect : String[];
+
+    //  Swords: Number;
+
+    //  Skill: Number;
+
+    //  Boots: Number;
+
+    //  VPTot: Number;
+
+    //  SkillCost: Number;
+
+    //  DragonAtkSmbl: Boolean;
+
+    //  Aquire: String[];
+
+    //  Arrive: String[];
+
+    //  Danger: String[];
+
+    //  FlavorText: String;
+
+}
